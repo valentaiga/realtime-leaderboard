89440f3f62d97175a3e86d8a957386798619a913
valentaiga <valenok146@gmail.com>
2026-02-23 23:53:35 +0300

    [Refactoring] ProtoFilesGen moved to Common folder

--- BE/BackOffice/Common.Tools.ProtoFilesGenerator/Generator.cs
+++ BE/BackOffice/Common.Tools.ProtoFilesGenerator/Generator.cs
@@ -1,11 +1,11 @@
 ﻿using ProtoBuf.Grpc.Reflection;
 using ProtoBuf.Meta;
 
-namespace BackOffice.Tools.ProtoFilesGen;
+namespace Common.Tools.ProtoFilesGenerator;
 
 public static class Generator
 {
-    public static async Task GenerateAsync(string interfaceName, Type[] types)
+    public static async Task GenerateAsync(string interfaceName, Type type)
     {
         Console.WriteLine($"ProtoFilesGen: Generating protobuf files for {interfaceName}...");
         var generator = new SchemaGenerator
@@ -15,7 +15,7 @@ public static class Generator
 
         var saveFolder = Path.Combine(FindSolutionDirectory(), "BackOffice", "ProtoFiles");
 
-        var output = generator.GetSchema(types);
+        var output = generator.GetSchema(type);
         var filePath = Path.Combine(saveFolder, $"{interfaceName}.proto");
         await File.WriteAllTextAsync(filePath, output);
         Console.WriteLine($"ProtoFilesGen: Proto file '{interfaceName}.proto' updated ({filePath})");
--- BE/BackOffice/Common.Tools.ProtoFilesGenerator/Program.cs
+++ BE/BackOffice/Common.Tools.ProtoFilesGenerator/Program.cs
@@ -1,21 +1,21 @@
 ﻿using BackOffice.Identity.Grpc;
-using BackOffice.Tools.ProtoFilesGen;
+using Common.Tools.ProtoFilesGenerator;
 
 Console.WriteLine("ProtoFilesGen: Args [" + string.Join(",", args) + "]");
 
 // ===================== update only this part =====================
 
-var grpcInterfaces = new Dictionary<string, Type[]>
+var grpcInterfaces = new Dictionary<string, Type>
 {
-    { "Identity.Grpc", [typeof(IIdentityApi)] }
+    { "Identity.Grpc", typeof(IIdentityApi) }
 };
 
 // ====================== do not update below ======================
 
 foreach (var interfaceName in args.Select(x => x[2..]))
 {
-    if (!grpcInterfaces.TryGetValue(interfaceName, out var types))
+    if (!grpcInterfaces.TryGetValue(interfaceName, out var type))
         throw new KeyNotFoundException("Project reference not found for proto file generation");
 
-    await Generator.GenerateAsync(interfaceName, types);
+    await Generator.GenerateAsync(interfaceName, type);
 }
