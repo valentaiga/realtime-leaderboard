015e5a9c342b4a0200e6a9bf069529596a36d3a6
valentaiga <valenok146@gmail.com>
2026-03-08 15:16:51 +0300

    [Refactoring] Migrations core to Common.Migrations.Postgres

--- BE/BackOffice/BackOffice.Chronicle.Migrations/Program.cs
+++ BE/BackOffice/BackOffice.Chronicle.Migrations/Program.cs
@@ -1,44 +1,11 @@
 ﻿using BackOffice.Chronicle.Migrations;
+using Common.Migrations.Postgres;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // builder.
-builder.Services
-    .AddDbContext<ChronicleDbContext>((sp, options) =>
-    {
-        var connectionString = builder.Configuration.GetConnectionString("ChronicleDb")
-                               ?? throw new InvalidOperationException("Connection string 'ChronicleDb' not configured");
-
-        sp.GetRequiredService<ILogger<ChronicleDbContext>>()
-            .LogInformation("Current connection is '{ConnectionString}'", connectionString);
-        options.UseNpgsql(connectionString, npgsqlOptions =>
-        {
-            npgsqlOptions.MigrationsHistoryTable("migrations_history");
-            npgsqlOptions.EnableRetryOnFailure(maxRetryCount: 3);
-            npgsqlOptions.CommandTimeout(30);
-        });
-    });
-
+builder.Services.AddMigrationsDbContext<ChronicleDbContext>(builder.Configuration, "ChronicleDb");
 var app = builder.Build();
 
-await EnsureMigrationAsync(app.Services);
-
-return;
-
-static async Task EnsureMigrationAsync(IServiceProvider serviceProvider)
-{
-    await using var scope = serviceProvider.CreateAsyncScope();
-    var context = scope.ServiceProvider.GetRequiredService<ChronicleDbContext>();
-
-    try
-    {
-        await context.Database.MigrateAsync();
-    }
-    catch (Exception ex)
-    {
-        var logger = scope.ServiceProvider.GetRequiredService<ILogger<ChronicleDbContext>>();
-        logger.LogError(ex, "Failed to apply migrations");
-        throw;
-    }
-}
+await app.Services.EnsureMigrationAsync<ChronicleDbContext>();
---
+++ BE/Common/Common.Migrations.Postgres/MigrationsDiExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Common.Migrations.Postgres;
+
+public static class MigrationsDiExtensions
+{
+    public static IServiceCollection AddMigrationsDbContext<TDbContext>(this IServiceCollection services, IConfiguration configuration, string connectionStringName) where TDbContext : DbContext
+    {
+        services.AddDbContext<TDbContext>((sp, options) =>
+        {
+            var connectionString = configuration.GetConnectionString(connectionStringName)
+                                   ?? throw new InvalidOperationException($"Connection string '{connectionStringName}' not configured");
+
+            sp.GetRequiredService<ILogger<TDbContext>>()
+                .LogInformation("Current connection is '{ConnectionString}'", connectionString);
+            options.UseNpgsql(connectionString, npgsqlOptions =>
+            {
+                npgsqlOptions.MigrationsHistoryTable("migrations_history");
+                npgsqlOptions.EnableRetryOnFailure(maxRetryCount: 3);
+                npgsqlOptions.CommandTimeout(30);
+            });
+        });
+
+        return services;
+    }
+
+    public static async Task EnsureMigrationAsync<TDbContext>(this IServiceProvider serviceProvider) where TDbContext : DbContext
+    {
+        await using var scope = serviceProvider.CreateAsyncScope();
+        var context = scope.ServiceProvider.GetRequiredService<TDbContext>();
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<TDbContext>>();
+
+        try
+        {
+            await context.Database.MigrateAsync();
+            logger.LogInformation("Migrated successfully");
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Failed to apply migrations");
+            throw;
+        }
+    }
+}
